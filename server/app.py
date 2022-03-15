e0024c4da51f489720aae411d0f8e5e3a5dcb32c
MaxDillon <maxdillon13124@gmail.com>
2022-03-15 13:15:33 -0700

    created connection between server and db (test with localhost/testQuery/[QUERY])

--- server/app.py
+++ server/app.py
@@ -1,16 +1,26 @@
 
 # DB_NAME = "mysql://root:password@localhost:3306"
 
+import sys
 from flask import Flask
 from blueprints import loginBlueprint
+from database.dbConnect import init_database, execute_query
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 database = {}
 
 
-app.register_blueprint(loginBlueprint.create_blueprint(database), url_prefix='/login')
+@app.route("/testQuery/<sql>")
+def hello(sql):
+	print(f"---------{sql}-----------", file=sys.stderr)
+	db = init_database()
+	execute_query(db, sql)
+	return ""
 
+
+
+app.register_blueprint(loginBlueprint.create_blueprint(database), url_prefix='/login')
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
--- server/database/dbConnect.py
+++ server/database/dbConnect.py
@@ -1,5 +1,38 @@
 
 
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://scott:tiger@localhost:5432/postgres')
+import sys
+from sqlalchemy import Column, MetaData, Table, create_engine, String
+import typing
 
+import sqlalchemy
+
+def init_database():
+	engine = create_engine('postgresql://postgres:postgres@postgres:5432/postgres', echo=True)
+	return engine
+
+
+def execute_query(database, sql):
+	result = database.execute(sql)
+	for r in result:
+		print(r, file=sys.stderr)
+
+
+
+def print_customers(database: sqlalchemy.engine.Engine):
+	meta = MetaData(database)
+	user_table = Table('', meta, 
+		Column('user_id', String),
+		Column('username', String),
+		Column('first_name', String),
+		Column('last_name', String),
+		Column('email', String),
+		Column('phone', sqlalchemy.Integer),
+		Column('pass_hash', String),
+		Column('money_invested', sqlalchemy.Integer)
+	)
+
+	with database.connect() as conn:
+		conn: sqlalchemy.engine.Connection
+		result = conn.execute(user_table.select())
+		for r in result:
+			print(r, file=sys.stderr)
