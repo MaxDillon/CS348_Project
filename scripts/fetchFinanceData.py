79c19aab6c5c97f2fbfa0491e99b1983c027376d
Pankaj <meghanipankaj5@gmail.com>
2022-04-27 16:06:27 -0400

    Cleanup

--- scripts/fetchFinanceData.py
+++ scripts/fetchFinanceData.py
@@ -1,4 +1,3 @@
-# %%
 import signal
 import psycopg2
 import numpy as np
@@ -6,8 +5,6 @@ import pandas as pd
 import time
 from tqdm import tqdm
 from yahoo_fin import stock_info as si
-import datetime
-from zoneinfo import ZoneInfo
 
 kill_now = False
 
@@ -18,8 +15,6 @@ def signalHandler(sig, frame):
 
 
 def fetchLoop():
-
-    stocks = ["MSFT", "GOOGL", "AMZN", "GS", "UBER"]
 
     con = psycopg2.connect(
         database="postgres",
@@ -43,7 +38,7 @@ def fetchLoop():
     print("stocks: ", stocks)
 
     while not kill_now:
-        # try:
+
         current_prices = {}
 
         for stock in stocks:
@@ -68,66 +63,12 @@ def fetchLoop():
         con.commit()
 
         time.sleep(60)
-        # except KeyboardInterrupt:
-        #     con.commit()
-        #     con.close()
-        #     break
+
+    con.commit()
+    con.close()
 
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signalHandler)
     signal.signal(signal.SIGTERM, signalHandler)
     fetchLoop()
-
-
-# # %%
-# # %%
-# fig = go.Figure(data=[
-#     go.Candlestick(
-#         x=list(df.index),
-#         open=df.Open,
-#         close=df.Close,
-#         high=df.High,
-#         low=df.Low
-#     )
-# ])
-
-# fig.update_xaxes(
-
-#     rangeslider_visible=True,
-#     rangebreaks=[
-#         # NOTE: Below values are bound (not single values), ie. hide x to y
-#         # hide weekends, eg. hide sat to before mon
-#         dict(bounds=["sat", "mon"]),
-#         # hide hours outside of 9.30am-4pm
-#         # dict(bounds=[16, 9.5], pattern="hour"),
-#         # dict(values=["2019-12-25", "2020-12-24"])  # hide holidays (Christmas and New Year's, etc)
-#     ],
-#     # rangeselector=dict(
-#     #     buttons=list([
-#     #         dict(count=1,
-#     #                  label="1m",
-#     #                  step="month",
-#     #                  stepmode="backward"),
-#     #         dict(count=6,
-#     #              label="6m",
-#     #              step="month",
-#     #              stepmode="backward"),
-#     #         dict(count=1,
-#     #              label="YTD",
-#     #              step="year",
-#     #              stepmode="todate"),
-#     #         dict(count=1,
-#     #              label="1y",
-#     #              step="year",
-#     #              stepmode="backward"),
-#     #         dict(step="all")
-#     #     ])
-#     # )
-
-# )
-
-
-# fig.update_layout(xaxis_rangeslider_visible=False)
-
-# fig.show()
