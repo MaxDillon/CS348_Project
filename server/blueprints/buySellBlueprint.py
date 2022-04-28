a1e1544319abd92c2e74bc7ad95d614d370fd755
Pankaj <meghanipankaj5@gmail.com>
2022-04-27 20:24:34 -0400

    returning stock data from server

--- server/blueprints/buySellBlueprint.py
+++ server/blueprints/buySellBlueprint.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, Session
 import flask
 from auth.auth_tools import login_required
 from database.schema import t_companyhistory
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
 
 
 def create_blueprint(Makesession: sessionmaker):
@@ -13,30 +15,49 @@ def create_blueprint(Makesession: sessionmaker):
     @login_required(Makesession)
     def getStockDetails():
 
+        response_body = {
+            "error": None,
+            "data": None
+        }
+
         session: Session = Makesession()
+        companyName: str = flask.request.args.get('company')
 
-        res = flask.make_response()
-        data = flask.request.args.get('company')
+        if companyName == None or companyName == "":
+            response_body["error"] = "No company name passed"
+            res = flask.make_response(flask.jsonify(response_body), 400)
+            return res
 
-        companyHistory = session.query(t_companyhistory).filter(
-            t_companyhistory.columns.company_id == "UBER")
+        companyName = companyName.upper()  # All company ids are in uppercase
 
-        # companyHistoryQuery = select(t_companyhistory.company_id, t_companyhistory.time_fetched,
-        #                              t_companyhistory.trading_price).where(t_companyhistory.company_id == data)
-        # companyHistoryQuery = select(t_companyhistory)
-        # companyHistory = Session.execute(companyHistoryQuery)
+        companyHistoryQuery = session.query(t_companyhistory.columns.time_fetched, t_companyhistory.columns.trading_price).filter(
+            t_companyhistory.columns.company_id == companyName)
 
-        # companyHistory: list[tuple] = session.query(
-        #     t_companyhistory.columns.company_id)
+        companyHistory = session.execute(companyHistoryQuery).all()
 
-        res = ""
-        for s in companyHistory:
-            res += s.__repr__() + '\t'
+        response = [x._asdict() for x in companyHistory]
 
-        # print(companyHistory)
+        response_body["data"] = response
+
+        res = flask.make_response(flask.jsonify(response_body), 200)
+        return res
+
+        # k = companyHistory[0]
+
+        # k = k._mapping
+
+        # res = ""
+        # for s in companyHistory:
+        #     res += s.__repr__() + '\t'
+
+        # print(companyHistory, flush=True)
+
+        # div = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
+
+        # return flask.Markup(div)
 
         # return companyHistory
 
-        return res
+        # return k.__repr__()
 
     return buySellBlueprint
