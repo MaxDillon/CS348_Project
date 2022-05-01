2b4c5129e2438d7e7344334b12b63cf8aaf16dc0
MaxDillon <maxdillon13124@gmail.com>
2022-05-01 01:58:56 -0400

    removed unneeded inverstor page functionality

--- client/src/Pages/InvestorPage/InvestorPage.js
+++ client/src/Pages/InvestorPage/InvestorPage.js
@@ -1,25 +1,12 @@
 import React from "react";
-import {useState, useLayoutEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
 
 
 
-async function logout() {
-	const res = await fetch('/auth/logout')
-	return res
-}
 
 export default function InvestorPage() {
-	const navigate = useNavigate()
-
-
 	return (
-	  <div className="App">
-		  <button onClick={e => {
-				e.preventDefault()
-				logout()
-				navigate("/login")
-		  }}>Logout</button>
+	  <div>
+		  <h1>Hey!</h1>
 	  </div>
 	);
  }
