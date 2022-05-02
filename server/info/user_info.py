449ca437bb157f5ee74058683bc33fabd92f3e05
MaxDillon <maxdillon13124@gmail.com>
2022-05-02 15:56:39 -0400

    adding python centric get_fund_fitness

--- server/info/user_info.py
+++ server/info/user_info.py
@@ -1,48 +1,107 @@
 from datetime import datetime
 import sys
 from sqlalchemy.orm import Session
-from sqlalchemy import alias, select, func
-from database.schema import Account, t_paymenthistory, t_companyhistory
+from sqlalchemy import Float, Integer, alias, select, func
+from database.schema import (
+    Account,
+    t_paymenthistory,
+    t_companyhistory,
+    t_fundperformance,
+)
 import numpy as np
+
+
+def get_fund_fitness(session: Session, time):
+    query = select(t_fundperformance).where(
+        t_fundperformance.columns.time_created == (
+            select(func.max(t_fundperformance.columns.ts)).where(
+                t_fundperformance.columns.ts <= time
+            )
+        )
+    )
+    result = session.execute(query).one_or_none()
+
+    return 0 if not result else result.fund_value
+
+
+def get_past_holdings(session: Session, account: Account, time_start: float, time_end: float):
+    time_start = 0
+    time_end = 100
+
+    times = []
+    values = []
+
+    last_invested = 0
+    last_investment = 0
+    last_fund_fitness = 0
+    for time in np.linspace(time_start, time_end, 500):
+        query = select(t_paymenthistory).where(
+            t_paymenthistory.columns.time_created == (
+                select(func.max(t_paymenthistory.columns.time_created)).where(
+                    t_paymenthistory.columns.time_created <= time
+                )
+            )
+        )
+        result = session.execute(query).one_or_none()
+        if result.time_created != last_invested:
+            last_invested = result.time_created
+            last_investment = result.money_invested
+            times.append(time)
+            values.append(result.money_invested)
+            last_fund_fitness = get_fund_fitness(session, result.time_created)
+            continue
+
+        
+        new_fund_fitness = get_fund_fitness(session, time)
+        times.append(time)
+        values.append(new_fund_fitness / last_fund_fitness * last_investment)
+
+        
+
+
+
+
+
 
 
 def get_past_holding_dump(
     session: Session, account: Account, time_start: float, time_end: float
 ):
 
-    # start_time, end_time = get_time_range(session, account, time_start, time_end)
-    # if start_time is None or end_time is None:
-    #     return None
+    time_start = 0
+    time_end = 100000
 
     time_created = t_paymenthistory.columns.time_created
-    amount_invested = t_paymenthistory.columns.amount_invested
-    user_id = t_paymenthistory.columns.user_id
-    time_fetched = t_companyhistory.columns.time_fetched
-    trading_price = t_companyhistory.columns.trading_price
-    company_id = t_companyhistory.columns.company_id
+    # amount_invested = t_paymenthistory.columns.amount_invested
+    # user_id = t_paymenthistory.columns.user_id
+    # time_fetched = t_companyhistory.columns.time_fetched
+    # trading_price = t_companyhistory.columns.trading_price
+    # company_id = t_companyhistory.columns.company_id
 
     ret = []
 
-    for latest_time in np.linspace(time_start, time_end, 100, False):
+    query = select(
+        t_fundperformance.columns.fund_value.cast(Float),
+        t_fundperformance.columns.ts,
+    ).where(
+        t_fundperformance.columns.ts >= time_start,
+        t_fundperformance.columns.ts <= time_end,
+    )
 
-        user_money_query = (
-            select(amount_invested, func.max(time_created))
-            .where(user_id == account.user_id, time_created <= latest_time)
-            .group_by(amount_invested)
+    ranged_performances = session.execute(query).all()
+    for performance in ranged_performances:
+
+
+        query = select(t_paymenthistory.columns.amount_invested.cast(Float), time_created).where(
+            time_created == (
+                select(func.max(time_created)).where(time_created <= performance.ts)
+            )
+            # , t_paymenthistory.columns.user_id == account.user_id
         )
+        current_investment = session.execute(query).fetchone()
 
-        latest_times = (
-            select(company_id, func.max(time_fetched).label("time"))
-            .where(time_fetched <= latest_time)
-            .group_by(company_id)
-            .subquery()
-        )
+        amount_invested = 0 if not current_investment else current_investment.amount_invested
 
-        total_holdings_query = select(func.sum(trading_price).label("price")).where(
-            company_id == latest_times.c.company_id, time_fetched == latest_times.c.time
-        )
-
-        res = session.execute(total_holdings_query).one()
-        ret.append((latest_time, res.price))
+        ret.append((amount_invested, performance.fund_value, float(performance.ts)))
 
     return ret
