3766448c484363549d50005392263843f6182e93
NamrataAcharya02 <nnachary@purdue.edu>
2022-05-03 00:15:35 -0400

    graph appears but not on reloads

--- client/src/Pages/FundInfoPage/FundInfo.js
+++ client/src/Pages/FundInfoPage/FundInfo.js
@@ -4,109 +4,51 @@ import { DateTime } from "luxon";
 
 export default function FundInfo() {
 
-    const companyID = "uber"
-
-    const [isLoading, setIsLoading] = useState(false)
     const [data, setData] = useState()
-    const [companyDetails, setCompanyDetails] = useState()
-
-    const [value, setValue] = useState(0);
-
-    /**
-     * 
-     * @param {int} value Number of stocks in transaction
-     * @param {boolean} buy Is it a buy or a sell transaction
-     */
-    const onSubmitHandler = async (value, buy) => {
-        const res = await fetch("/buySell/", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ buy, "value": value })
-        })
-        const parsedResponse = await res.json()
-
-        if (parsedResponse.ok == false) {
-            alert(parsedResponse.error)
-        }
-        else {
-            alert("Transaction successful")
-            // return to previous page
-        }
-    }
-
-    const sample = {
-        data: {
-          companyDetails: {
-            company_name: "Uber Technologies Inc", 
-            num_shares: 0
-          }, 
-          stockData: [{time: 1, fund: 12}, {time: 2, fund: 14}, {time: 3, fund: 25}]
-        }, 
-        "error": null
-    }
-
-    const times = sample.data.stockData.map(row => {return row.time});
-    const fundVal = sample.data.stockData.map(row => {return row.fund});
-
-   /* useEffect(() => {
-        setData(() => ([{
-            x: times,
-            y: fundVal,
-            type: 'scatter'
-        }]))
-
-    }, [])   */ 
-
-
+    const [received, setReceived] = useState({times: [], values: []})
+    const [isLoading, setIsLoading] = useState(true)
+    const [times, setTimes] = useState([])
+    const [values, setValues] = useState([])
 
     useEffect(() => {
         (async () => {
             const response = await fetch("/money/pastHoldings")
             .then(res => res.json())
             .then( data => {
-                setData(data)
+                setReceived(data)
             })
-            // parsedResponse: {data: {time_fetched:int, trading_price:str}[], error: str}
+            //console.log(received)
 
-           /* const dates = parsedResponse.data.stockData.map(row => row.time_fetched).map(ts => {
-                return DateTime
-                    .fromSeconds(ts).setZone("America/New_York")
-                    .toFormat("kkkk-LL-dd HH:mm:ss")
-                // https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-                // https://plotly.com/javascript/time-series/
-            })*/
-            const times = data.times;
-
-            const values = data.values;
-
-            // console.log(date_data)
+            const times = received.times;
+            const values = received.values;
+           
             setData(() => ([{
                 x: times,
                 y: values,
                 type: 'scatter'
-            }]))
-
-            
-            console.log(data)
+            }])) 
 
             setIsLoading(false)
+
+
         })()
     }, []
     ) 
 
 
+
     return <div>
         {
-            !isLoading
-                ? (<>
-                    <Plot data={data} style={{
-                        display: "block", width: "80%", margin: "0 auto"
-                    }} />
+           !isLoading
+           ? (<>
+          <h1 >Fund History</h1>
+          <h2>View values over a time period</h2>
+                    <Plot data={data} 
+                        style={{
+                            display: "block", width: "100%", margin: "0 auto"
+                         }} />
                     <div style={{
-                        width: "80%",
+                        width: "100%",
                         display: "flex",
                         flexDirection: "row",
                         justifyContent: "space-evenly",
@@ -114,9 +56,8 @@ export default function FundInfo() {
                         margin: "0 auto"
                     }}>
                     </div>
-                </>
-                )
-                : "Loading"
+            </>): "Loading"
+                
         }
     </div>
 }
