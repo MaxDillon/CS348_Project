630c9ade4954bd05d7b7f2bf7b99bf2bdba3db49
MaxDillon <maxdillon13124@gmail.com>
2022-05-03 01:57:51 -0400

    cleaned up fundInfo

--- client/src/Pages/FundInfoPage/FundInfo.js
+++ client/src/Pages/FundInfoPage/FundInfo.js
@@ -1,63 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Plot from 'react-plotly.js';
-import { DateTime } from "luxon";
 
 export default function FundInfo() {
 
     const [data, setData] = useState()
-    const [received, setReceived] = useState({times: [], values: []})
     const [isLoading, setIsLoading] = useState(true)
-    const [times, setTimes] = useState([])
-    const [values, setValues] = useState([])
 
     useEffect(() => {
-        (async () => {
-            const response = await fetch("/money/pastHoldings")
-            .then(res => res.json())
-            .then( data => {
-                setReceived(data)
-            })
-            //console.log(received)
-
-            const times = received.times;
-            const values = received.values;
-           
-            setData(() => ([{
-                x: times,
-                y: values,
+        fetch("/money/pastHoldings")
+        .then(res => res.json())
+        .then( data => {
+            console.log(data)
+            setData({
+                x: data.times,
+                y: data.values,
                 type: 'scatter'
-            }])) 
-
+            })
             setIsLoading(false)
+        })
 
-
-        })()
-    }, []
-    ) 
+    }, []) 
 
 
 
-    return <div>
-        {
-           !isLoading
-           ? (<>
-          <h1 >Fund History</h1>
-          <h2>View values over a time period</h2>
-                    <Plot data={data} 
-                        style={{
-                            display: "block", width: "100%", margin: "0 auto"
-                         }} />
-                    <div style={{
-                        width: "100%",
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "space-evenly",
-                        alignItems: "center",
-                        margin: "0 auto"
-                    }}>
-                    </div>
-            </>): "Loading"
-                
-        }
-    </div>
+    return (<>
+        {!isLoading? (<>
+            <h1 >Fund History</h1>
+            <h2>View values over a time period</h2>
+            <Plot data={[data]} 
+                style={{
+                    display: "block", width: "100%", margin: "0 auto", listStylePosition: "relative"
+                    }} />
+        </>): "Loading" }
+    </>)
 }
